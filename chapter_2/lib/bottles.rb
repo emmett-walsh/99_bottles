3ccfebfb1fe994f2893e148e5d47a5e5748466c7
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2017-07-27 20:44:00 +0100

    Extracts BottleNumber class and add Guard to gemfile
    Guard is broken.
    Create call to new instance of BottleNumber for each of the flocking five methods.

--- chapter_2/lib/bottles.rb
+++ chapter_2/lib/bottles.rb
@@ -15,6 +15,34 @@ class Bottles
   end
 
   def container(number)
+    BottleNumber.new(number).container
+  end
+
+  def pronoun(number)
+    BottleNumber.new(number).pronoun
+  end
+
+  def quantity(number)
+    BottleNumber.new(number).quantity
+  end
+
+  def action(number)
+    BottleNumber.new(number).action
+  end
+
+  def successor(number)
+    BottleNumber.new(number).successor
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def container
     if number == 1
       "bottle"
     else
@@ -22,7 +50,7 @@ class Bottles
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       "it"
     else
@@ -30,7 +58,7 @@ class Bottles
     end
   end
 
-  def quantity(number)
+  def quantity
     if number == 0
       "no more"
     else
@@ -38,15 +66,15 @@ class Bottles
     end
   end
 
-  def action(number)
+  def action
     if number == 0
       "Go to the store and buy some more"
     else
-      "Take #{pronoun(number)} down and pass it around"
+      "Take #{pronoun} down and pass it around"
     end
   end
 
-  def successor(number)
+  def successor
     if number == 0
       99
     else
